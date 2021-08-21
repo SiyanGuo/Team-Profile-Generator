a5a229675a3001d870230f6b36440071e96e36e5
Siyan Guo <siyang@Hey-There.local>
2021-08-21 19:08:48 -0400

    test Employee class

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,30 @@
+const Employee = require('../lib/Employee');
+
+test('creates a employee object', ()=>{
+    const employee = new Employee('Lisa', 1, 'e@t.com');
+
+    expect(employee.name).toBe('Lisa');
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.email).toEqual(expect.stringContaining('.com'));
+});
+
+test("get employee's name", ()=>{
+    const employee = new Employee('Lisa', 1, 'e@t.com');
+    expect(employee.getName()).toBe('Lisa');
+});
+
+test("get employee's id", ()=>{
+    const employee = new Employee('Lisa', 1, 'e@t.com');
+    expect(employee.getId()).toBe(1);
+});
+
+test("get employee's email", ()=>{
+    const employee = new Employee('Lisa', 1, 'e@t.com');
+    expect(employee.getEmail()).toBe('e@t.com');
+});
+
+test("get employee's role", ()=>{
+    const employee = new Employee('Lisa', 1, 'e@t.com');
+    expect(employee.getRole() ).toBe('Employee');
+});
+
